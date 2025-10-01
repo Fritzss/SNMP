39c4fa73d33fcb87c39bdaa441cb92e5bf6ae8a9
Fritzss <Sergey.Nik.Krylov@gmail.com>
2025-10-01 15:30:43 +0300

    Update generate_snmp.sh

--- generate_snmp.sh
+++ generate_snmp.sh
@@ -1,4 +1,7 @@
+CURRENTPATH=$(pwd)
+PATHSNMP="$CURRENTPATH/mik_snmp.yml"
+echo "$PATHSNMP"
 git clone https://github.com/prometheus/snmp_exporter.git
-cd snmp_exporter/generator/
+cd ./snmp_exporter/generator/
 make generate mibs
-./generator generate -m mibs/ -o ./mik_snmp.yml
+./generator generate -m "$CURRENTPATH/mibs/" -g "$CURRENTPATH/generator.yml" -o "$PATHSNMP"
